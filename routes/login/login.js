f549fd90498678b79ae01f0a318c1a636209f360
jieun69 <jiehae986@naver.com>
2022-04-28 09:44:49 -0700

    .

--- routes/login/login.js
+++ routes/login/login.js
@@ -1,2 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const controller = require('./login.controller');
+
+router.get('/', controller.login);
