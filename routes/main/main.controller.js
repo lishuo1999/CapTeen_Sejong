4efe249313013489821b530278a9549a15eb0f9f
touBVa <touBVa@gmail.com>
2022-05-02 11:14:24 +0900

    수정

--- routes/main/main.controller.js
+++ routes/main/main.controller.js
@@ -2,6 +2,4 @@
 
 exports.main = (req, res, next) => {
     res.sendFile('index.html', {root: 'public/htmls'});
-    res.sendFile('index.css', {root: 'public/htmls'});
-    res.sendFile('indexvideo.mp4', {root: 'public/htmls'});
 }
