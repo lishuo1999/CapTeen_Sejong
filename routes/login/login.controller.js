9cc7b50adf759b8197257ddde490448b96b9cc67
touBVa <touBVa@gmail.com>
2022-05-16 22:36:48 -0700

    login_prototype2

--- routes/login/login.controller.js
+++ routes/login/login.controller.js
@@ -1,19 +1,20 @@
 const db= require('../serverConfig');
 const crypto=require('crypto');
 const session = require('express-session');
+const { resolve } = require('path');
 
-exports.login = (req, res, next) => {
+exports.login = async(req, res, next) => {
     const usr_id=req.body.userId.replace("'", "");
     const usr_pw=req.body.userPw.replace("'", "");
     const usr_pw_sha512 = crypto.createHash('sha512').update(usr_pw).digest('hex');
     var queryResult=0;
     //find whether there is a user matches id and pw in the mysql.user
-    queryResult = isThereUser(usr_id, usr_pw_sha512);
+    queryResult = await(isThereUser(usr_id, usr_pw_sha512));
     //if there's a user then grant user a session
     if(queryResult==1){
         req.session.displayName=usr_id;
         req.session.loginData=req.session.save(error=>{if(error) console.log(error)});
-        res.json({message : 'login success'});
+        res.sendFile('Anal_1.html', {root: 'public/htmls'});
     }
 
     //else if there's a user matches with id info, then show alert "id or pw is incorrect"
@@ -24,20 +25,24 @@ exports.login = (req, res, next) => {
 }
 
 //function which searches whether there's a user matches with the given parameter(id, pw)
-function isThereUser(id, pw){
-    const searchSql='SELECT usr_id, usr_pw FROM usr_db.users WHERE usr_id=?';
-    db.query(searchSql, id, function(err, rows, field){
-        if(err) console.log(err);
-        else if(rows.length>0) //id exists
-        {
-            if(rows.usr_pw == pw) //user certificated
+isThereUser=(id, pw)=>{
+    return new Promise(async (resolve, reject) => {
+        const searchSql='SELECT usr_id, usr_pw FROM usr_db.users WHERE usr_id=?';
+        db.query(searchSql, id, function(err, rows, field){
+            if(err) console.log(err);
+            console.log(rows);
+            if(rows.length>0) //id exists
             {
-                return 1;
+                if(rows[0].usr_pw == pw) //user certificated
+                {
+                    resolve(1);
+                }
+                else{//password is wrong
+                    resolve(0);
+                }
             }
-            else{//password is wrong
-                return 0;
-            }
-        }
-        else return -1; //id is not exist
+            else resolve(-1); //id is not exist
+        })
     })
+
 }
