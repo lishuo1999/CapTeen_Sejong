770adbc6e3518f8ebc47975568934f37654241c7
touBVa <touBVa@gmail.com>
2022-05-29 02:20:06 -0700

    aef

--- routes/serverConfig.js
+++ routes/serverConfig.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 //should change below after the real world implementation
 const db = mysql.createConnection({
-    host : "14.40.31.222",
+    host : "localhost",
     user : 'root', //for now it is the root user, but gotta make a new user with limited privileged role 
     password:'password',
     port:3306,
